1d5a7f5575202c62fc3db047db6d6edf7308dee6
CoderSaB <sandesh.bdyl.2060@gmail.com>
2025-05-21 22:12:13 +0545

    changes for dropdowns

---
+++ index.js
@@ -0,0 +1,38 @@
+// dropdown of category & blog list items
+const CategoryDropdownContainer = document.getElementById("category-dropdown-container");
+const BlogDropdownContainer = document.getElementById("blog-dropdown-container");
+
+const CategoryIcon = document.getElementById("category-dropdown-icon");
+const BlogIcon = document.getElementById("blog-dropdown-icon");
+
+CategoryIcon.addEventListener("click", () => {
+    CategoryIcon.style.transform = "rotateX(180deg)";
+    CategoryIcon.style.transition = "0.5s ease-in-out";
+    CategoryDropdownContainer.style.display = "flex";  
+})
+document.addEventListener("mousedown", () => {
+    CategoryIcon.style.transform = "rotateX(0deg)";
+    CategoryIcon.style.transition = "0.5s ease-in-out";
+    CategoryDropdownContainer.style.display = "none";  
+})
+CategoryIcon.addEventListener("mouseenter", () => {
+    CategoryIcon.style.transform = "rotateX(0deg)";
+    CategoryIcon.style.transition = "0.5s ease-in-out";
+    CategoryDropdownContainer.style.display = "none";  
+})
+BlogIcon.addEventListener("click", () => {
+    BlogIcon.style.transform = "rotateX(180deg)";
+    BlogIcon.style.transition = "0.5s ease-in-out";
+    BlogDropdownContainer.style.display = "flex";  
+})
+document.addEventListener("mousedown", () => {
+    BlogIcon.style.transform = "rotateX(0deg)";
+    BlogIcon.style.transition = "0.5s ease-in-out";
+    BlogDropdownContainer.style.display = "none";  
+})
+BlogIcon.addEventListener("mouseenter", () => {
+    BlogIcon.style.transform = "rotateX(0deg)";
+    BlogIcon.style.transition = "0.5s ease-in-out";
+    BlogDropdownContainer.style.display = "none";  
+})
+// dropdown of category & blog list items ends here
